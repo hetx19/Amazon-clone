e4b2222f4936201c293f446bb589b01bdf1f0bd3
Harshal Savaliya <hetsavaliya19@gmail.com>
2022-09-25 13:15:15 +0530

    Amazon clone using React and Firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBKfA-ILAnCpeoxmqpQEmLx7G4dll8t7qY",
+    authDomain: "clone-b9046.firebaseapp.com",
+    projectId: "clone-b9046",
+    storageBucket: "clone-b9046.appspot.com",
+    messagingSenderId: "491745233037",
+    appId: "1:491745233037:web:2018b21898b906523c7ac4",
+    measurementId: "G-KHQQQP1VGQ"
+};
