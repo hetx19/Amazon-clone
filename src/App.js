530979623dd41f0d9dd689b3d2818a02c1170cd6
Harshal Savaliya <hetsavaliya19@gmail.com>
2022-09-25 14:28:15 +0530

    Connecting firebase and react frontend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './components/Header';
 import Home from './components/Home';
 import Checkout from './components/Checkout';
+import Login from './components/Login';
+import { useStateValue } from './components/StateProvider';
+import { auth } from './firebase';
 
 function App() {
+
+  const [{ }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className="app">
         <Header />
         <Switch>
           <Route exact path="/login">
-            Login Page
+            <Login />
           </Route>
           <Route exact path="/checkout">
             <Checkout />
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyBKfA-ILAnCpeoxmqpQEmLx7G4dll8t7qY",
@@ -8,3 +12,10 @@ const firebaseConfig = {
     appId: "1:491745233037:web:2018b21898b906523c7ac4",
     measurementId: "G-KHQQQP1VGQ"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
